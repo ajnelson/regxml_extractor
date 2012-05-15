37ae6dfb4d38ca13ea0eb2224ec614116ced2700
Alex Nelson <ajnelson@cs.ucsc.edu>
2012-05-14 23:26:09 -0700

    Start processing script

---
+++ scripts/regxmlify_image.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Invoke extraction script
+# For each hive file extracted, run hivexml
+# For each regxml file generated, run xmllint to pretty-print
+# When all regxml is pretty-printed, generate a database
