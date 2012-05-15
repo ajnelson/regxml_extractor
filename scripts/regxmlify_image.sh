595d6bcbea3691fb3eb63a9e793514b10f492a1c
Alex Nelson <ajnelson@cs.ucsc.edu>
2012-05-14 23:43:53 -0700

    Add initial workflow script

--- scripts/regxmlify_image.sh
+++ scripts/regxmlify_image.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
+if [ $# -lt 2 ]; then
+  echo "Usage: $0 <disk image>" >&2
+  exit 1
+fi
+
+thisscriptdir=$(dirname $0)
+
 # Invoke extraction script
-# For each hive file extracted, run hivexml
+$thisscriptdir/../python/extract_hives.py --hivexml "$1" >manifest.txt
 # For each regxml file generated, run xmllint to pretty-print
+rm -f linted.txt
+for x in $(ls *hive); do xmllint --format "$x" >"${x}.pretty.regxml" && echo $x >>linted.txt ; done
 # When all regxml is pretty-printed, generate a database
+$thisscriptdir/../python/make_database.py linted.txt manifest.txt out.sqlite
