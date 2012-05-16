f5204acd3bb7605bfa0303497cb9a0bb505b725f
Alex Nelson <ajnelson@cs.ucsc.edu>
2012-05-16 10:21:01 -0700

    Remove script pathing

--- scripts/regxmlify_image.sh
+++ scripts/regxmlify_image.sh
@@ -5,12 +5,10 @@ if [ $# -lt 1 ]; then
   exit 1
 fi
 
-thisscriptdir=$(dirname $0)
-
 # Invoke extraction script
-$thisscriptdir/../python/extract_hives.py --hivexml "$1" >manifest.txt
+extract_hives.py --hivexml "$1" >manifest.txt
 # For each regxml file generated, run xmllint to pretty-print
 rm -f linted.txt
 for x in $(ls *hive); do xmllint --format "$x" >"${x}.pretty.regxml" && echo $x >>linted.txt ; done
 # When all regxml is pretty-printed, generate a database
-$thisscriptdir/../python/make_database.py linted.txt manifest.txt out.sqlite
+make_database.py linted.txt manifest.txt out.sqlite
