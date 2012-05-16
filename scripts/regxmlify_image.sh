aa4ef5fb371daaa2d59fcc47c77c9fff1ab27c77
Alex Nelson <ajnelson@cs.ucsc.edu>
2012-05-16 12:14:56 -0700

    Add partial run check, correct hivexml call, add error logging

--- scripts/regxmlify_image.sh
+++ scripts/regxmlify_image.sh
@@ -11,14 +11,25 @@ if test "x$(which extract_hives.py)" = "x" ; then
   SCRIPT_PREFIX="./"
 fi
 
-# Invoke extraction script
-"${SCRIPT_PREFIX}extract_hives.py" --hivexml "$1" >manifest.txt
+#Does it look like we ran before?
+if [ $(ls *hive | wc -l) -gt 0 ]; then
+  echo "Found some hive files.  Assuming extraction has already run.  If this is wrong, remove *.hive"
+else
+  #Invoke extraction script
+  "${SCRIPT_PREFIX}extract_hives.py" --hivexml "$1" >manifest.txt
+fi
+
 # For each regxml file generated, run xmllint to validate and pretty-print
 rm -f linted.txt
 if [ $(ls *hive | wc -l) -eq 0 ]; then
   echo "No hives extracted."
 else
-  for x in $(ls *hive); do xmllint --format "$x" >"${x}.checked.regxml" && echo $x >>linted.txt ; done
+  for x in $(ls *hive);
+  do
+    hivexml "${x}" >"${x}.regxml" 2>${x}.hivexml.err.log && \
+    xmllint --format "${x}.regxml" >"${x}.checked.regxml" 2>${x}.xmllint.err.log&& \
+    echo $x >>linted.txt ;
+  done
   # When all regxml is pretty-printed, generate a database
   if [ -f linted.txt ]; then
     "${SCRIPT_PREFIX}make_database.py" linted.txt manifest.txt out.sqlite
