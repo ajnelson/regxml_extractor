94161cf0926a331fea15f7134370ef25174e6b84
Alex Nelson <ajnelson@cs.ucsc.edu>
2012-05-16 15:16:13 -0700

    Correct script references

--- scripts/rx_regxml_from_image.sh
+++ scripts/rx_regxml_from_image.sh
@@ -7,7 +7,7 @@ fi
 
 #If not installed, just run local scripts
 SCRIPT_PREFIX=""
-if test "x$(which extract_hives.py)" = "x" ; then
+if test "x$(which rx_extract_hives.py)" = "x" ; then
   SCRIPT_PREFIX="./"
 fi
 
@@ -16,7 +16,7 @@ if [ $(ls *hive 2>/dev/null | wc -l) -gt 0 ]; then
   echo "Found some hive files.  Assuming extraction has already run.  If this is wrong, remove *.hive"
 else
   #Invoke extraction script
-  "${SCRIPT_PREFIX}extract_hives.py" --hivexml "$1" >manifest.txt
+  "${SCRIPT_PREFIX}rx_extract_hives.py" --hivexml "$1" >manifest.txt
 fi
 
 # For each regxml file generated, run xmllint to validate and pretty-print
@@ -32,6 +32,6 @@ else
   done
   # When all regxml is pretty-printed, generate a database
   if [ -f linted.txt ]; then
-    "${SCRIPT_PREFIX}make_database.py" linted.txt manifest.txt out.sqlite
+    "${SCRIPT_PREFIX}rx_make_database.py" linted.txt manifest.txt out.sqlite
   fi
 fi
