64715088341181e0b01632ac62ecfbc104174142
Alex Nelson <ajnelson@cs.ucsc.edu>
2012-05-16 10:31:58 -0700

    Add processing checks

--- scripts/regxmlify_image.sh
+++ scripts/regxmlify_image.sh
@@ -7,8 +7,14 @@ fi
 
 # Invoke extraction script
 extract_hives.py --hivexml "$1" >manifest.txt
-# For each regxml file generated, run xmllint to pretty-print
+# For each regxml file generated, run xmllint to validate and pretty-print
 rm -f linted.txt
-for x in $(ls *hive); do xmllint --format "$x" >"${x}.pretty.regxml" && echo $x >>linted.txt ; done
-# When all regxml is pretty-printed, generate a database
-make_database.py linted.txt manifest.txt out.sqlite
+if [ $(ls *hive | wc -l) -eq 0 ]; then
+  echo "No hives extracted."
+else
+  for x in $(ls *hive); do xmllint --format "$x" >"${x}.checked.regxml" && echo $x >>linted.txt ; done
+  # When all regxml is pretty-printed, generate a database
+  if [ -f linted.txt ]; then
+  make_database.py linted.txt manifest.txt out.sqlite
+  fi
+fi
