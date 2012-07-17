c4166b9ff671e307911e691fced1d3b82ea74252
Alex Nelson <ajnelson@cs.ucsc.edu>
2012-07-17 16:02:33 -0400

    Add unit test

--- scripts/rdifference_db.py
+++ scripts/rdifference_db.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
 
+__version__ = "0.0.0"
+
 import sys,fiwalk,dfxml,time
 if sys.version_info < (3,1):
     raise RuntimeError("rdifference_db.py requires Python 3.1 or above")
 import os
 import argparse
-import rdifference
+#import rdifference
+
+import collections
 
 #TODO Convert this to provide content to Postgres
 import sqlite3
@@ -21,6 +25,7 @@ def parse_sequence_map(map_file):
     For example, this content:
         
         project0    /foo/project0_0.reout   /bar/project0_0.dfxml
+        project1    /foo/project1_0.reout   /bar/project1_0.dfxml
         project0    /foo/project0_1.reout   /bar/project0_1.dfxml
     
     Would become:
@@ -30,6 +35,10 @@ def parse_sequence_map(map_file):
             "sequence":"project0",
             "dfxml":"/bar/project0_0.dfxml"
           },
+          "/foo/project1_0.reout":{
+            "sequence":"project1",
+            "dfxml":"/bar/project1_0.dfxml"
+          },
           "/foo/project0_1.reout":{
             "sequence":"project0",
             "dfxml":"/bar/project0_1.dfxml"
@@ -37,8 +46,10 @@ def parse_sequence_map(map_file):
         }
     
     Non-blank lines have a strict syntax enforcement check.
+    
+    Output directories must be unique.
     """
-    retval = {}
+    retval = collections.OrderedDict()
     for (enumline, line) in enumerate(map_file):
         clean_line = line.strip()
         if clean_line != "":
@@ -75,10 +86,33 @@ def process_hive_sequence(hive_sequence):
 if __name__ == "__main__":
     global args
     parser = argparse.ArgumentParser(description="Format the Registry differences of a sequence of RegXML Extractor output directories.")
-    parser.add_argument("sequence_map", type=argparse.FileType("r") help="Tab-delimited file mapping sequence name to regxml_extractor.sh output directories; optional and _strongly encouraged_ third column specifies DFXML of disk image")
+    parser.add_argument("sequence_map", type=argparse.FileType("r"), help="Tab-delimited file mapping sequence name to regxml_extractor.sh output directories; optional and _strongly encouraged_ third column specifies DFXML of disk image")
     parser.add_argument("re_out", nargs="+", help="regxml_extractor.sh output directories, in sequence order")
+    parser.add_argument("--regress", action="store_true", help="Run unit tests and exit")
     parser.add_argument("-d", "--debug", help="Enable debug-level logging", action="store_true")
     args = parser.parse_args()
+    
+    if args.regress:
+        from io import StringIO
+        test_sequence_input = """project0\t/foo/project0_0.reout\t/bar/project0_0.dfxml
+project1\t/foo/project1_0.reout\t/bar/project1_0.dfxml
+project0\t/foo/project0_1.reout\t/bar/project0_1.dfxml"""
+        test_sequence_dict = parse_sequence_map(test_sequence_input)
+        assert test_sequence_dict == {
+              "/foo/project0_0.reout":{
+              "sequence":"project0",
+              "dfxml":"/bar/project0_0.dfxml"
+            },
+              "/foo/project1_0.reout":{
+              "sequence":"project1",
+              "dfxml":"/bar/project1_0.dfxml"
+            },
+              "/foo/project0_1.reout":{
+              "sequence":"project0",
+              "dfxml":"/bar/project0_1.dfxml"
+            }
+        }
+
     
     if len(args.re_out) < 2:
         raise Exception("This script can only be invoked on more than one regxml_extractor.sh output directory.")
