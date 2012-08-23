f1ed936b2b30095fe4d1995c63a206a5ac509891
Alex Nelson <ajnelson@cs.ucsc.edu>
2012-08-23 13:54:08 -0700

    Add a builder script

---
+++ deps/make-dependencies.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ -z "$prefix" ]; then
+  prefix=$HOME/local
+fi
+
+pushd hivex
+(./autogen.sh && ./configure && make) || exit 1
+popd
+
+pushd sleuthkit
+(./bootstrap.sh && ./configure --prefix="$prefix" && make && make install) || exit 1
+popd
