9f93bd1c3ebee7e0c83067a1580a673049d63134
Alex Nelson <ajnelson@cs.ucsc.edu>
2012-08-28 17:13:36 -0700

    Add dependency-building script
    Helpful for gitbuilder. May obviate some documentation.
    
    Current as of commit 210be993f43b6b597e3021a5897e2adc4703a353.

---
+++ deps/make-dependencies.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ -z "$prefix" ]; then
+  prefix=$HOME/local
+fi
+
+pushd hivex
+(./autogen.sh && ./configure --disable-ruby && make) || exit 1
+popd
+
+pushd sleuthkit
+(./bootstrap && ./configure --prefix="$prefix" && make -j && make install) || exit 1
+popd
