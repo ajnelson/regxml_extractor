5ece6255c379edfe24c2697997540a356a628af4
Alex Nelson <ajnelson@cs.ucsc.edu>
2012-12-01 13:37:43 -0500

    Fix relative directory reference

--- deps/make-dependencies.sh
+++ deps/make-dependencies.sh
@@ -4,10 +4,13 @@ if [ -z "$prefix" ]; then
   prefix=$HOME/local
 fi
 
-pushd hivex
+#One-liner c/o http://stackoverflow.com/a/246128/1207160
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+pushd $SCRIPTDIR/hivex
 (./autogen.sh && ./configure --disable-ruby --disable-python --prefix="$prefix" && make -j && make install) || exit 1
 popd
 
-pushd sleuthkit
+pushd $SCRIPTDIR/sleuthkit
 (./bootstrap && ./configure --prefix="$prefix" && make -j && make install) || exit 1
 popd
