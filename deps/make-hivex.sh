3d0896f23a0308c9745d1b859729553b9ff9dce5
Alex Nelson <ajnelson@cs.ucsc.edu>
2012-12-01 15:02:51 -0500

    Break up build scripts

--- deps/make-hivex.sh
+++ deps/make-hivex.sh
@@ -11,6 +11,7 @@ fi
 #One-liner c/o http://stackoverflow.com/a/246128/1207160
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-prefix="$prefix" $SCRIPTDIR/make-hivex.sh
+pushd $SCRIPTDIR/sleuthkit
+(./bootstrap && ./configure --prefix="$prefix" && make -j && make install) || exit 1
+popd
 
-prefix="$prefix" $SCRIPTDIR/make-sleuthkit.sh
