6439b7e34bfdb4cd4aba374572e507b0290bf468
Alex Nelson <a.nelson@prometheuscomputing.com>
2013-04-04 11:56:07 -0400

    Correct name reversal

--- deps/make-sleuthkit.sh
+++ deps/make-sleuthkit.sh
@@ -11,7 +11,7 @@ fi
 #One-liner c/o http://stackoverflow.com/a/246128/1207160
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-pushd $SCRIPTDIR/hivex
-(./autogen.sh && ./configure --disable-ruby --disable-python --prefix="$prefix" && make -j && make install) || exit 1
+pushd $SCRIPTDIR/sleuthkit
+(./bootstrap && ./configure --prefix="$prefix" && make -j && make install) || exit 1
 popd
 
