174adbe30763c72735a2789d0b786ebd8b6bde28
Alex Nelson <ajnelson@cs.ucsc.edu>
2013-04-04 11:43:21 -0400

    Import build_submodules script strategy

---
+++ deps/build_submodules.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+#Halt on any error
+set -e
+set -v
+
+#TODO Get realpath of $INSTALLDIR
+case $1 in
+  local )
+    MAKEINSTALL="make install"
+    if [ -z "$2" ]; then
+      INSTALLDIR=$HOME/local
+    else
+      INSTALLDIR=$2
+    fi
+    ;;
+  system )
+    MAKEINSTALL="sudo make install"
+    INSTALLDIR=/usr/local
+    ;;
+  * )
+    echo "Error: Arguments must be 'local [opt. prefix_dir]', or 'system'" >&2
+    exit 1
+    ;;
+esac
+
+#One-liner c/o http://stackoverflow.com/a/246128/1207160
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+prefix="$INSTALLDIR" $SCRIPTDIR/make-hivex.sh
+
+prefix="$INSTALLDIR" $SCRIPTDIR/make-sleuthkit.sh
