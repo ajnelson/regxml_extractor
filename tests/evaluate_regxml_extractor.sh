9bfb0d8dc589d8c1d7c41cd520546f29e351265b
Alex Nelson <a.nelson@prometheuscomputing.com>
2013-05-24 16:10:08 -0400

    Abstract out a path

--- tests/evaluate_regxml_extractor.sh
+++ tests/evaluate_regxml_extractor.sh
@@ -13,16 +13,15 @@ if [ ! -r "$imglist" ]; then
 fi
 
 #TODO Change pwd to here, to be in Git source directory
-githead=$(git rev-parse HEAD)
+outdir_root=evaluations_by_commit/$(git rev-parse HEAD)
 
-mkdir -p evaluations_by_commit/$githead
+mkdir -p "$outdir_root"
 while read x; do
   if [ -r "$x.dfxml" ]; then
-    bn=$(basename "$x")
-    outdir=evaluations_by_commit/$githead/$bn
+    outdir=${outdir_root}/$(basename "$x")
     regxml_extractor.sh -d -o "$outdir" -x "$x.dfxml" "$x" >"${outdir}.out.log" 2>"${outdir}.err.log"
     echo $? >${outdir}.status.log
   fi
 done <"$imglist"
 
-echo "Number of disk images processing errors: $(grep -v '0' evaluations_by_commit/$githead/*.status.log | wc -l)"
+echo "Number of disk images processing errors: $(grep -v '0' ${outdir_root}/*.status.log | wc -l)"
