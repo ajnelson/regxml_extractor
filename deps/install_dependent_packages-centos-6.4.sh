18f98ee41b2cf9af33011aef0bd76e5efe49a1a9
Alex Nelson <a.nelson@prometheuscomputing.com>
2013-05-28 13:32:42 -0400

    Add package-tracking scripts
    Signed-off-by: Alex Nelson <a.nelson@prometheuscomputing.com>

---
+++ deps/install_dependent_packages-centos-6.4.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+if [ ! -r /etc/centos-release ]; then
+  echo "This does not appear to be a Centos machine." >&2
+  exit 1
+fi
+
+#Install sqlite-devel before configuring Python 3
+sudo yum install sqlite-devel
+
+p3=`which python3`
+if [ ! -x "$p3" ]; then
+  echo "Error: Centos 6.4 does not package Python 3.  You must install it from source." >&2
+  echo "  Reference:  http://www.hosting.com/support/linux/installing-python-3-on-centosredhat-5x-from-source" >&2
+  exit 1
+fi
+
+python3 -c 'import sqlite3'
+rcpy3sqlite3=$?
+if [ ! $rcpy3sqlite3 -eq 0 ]; then
+  echo "Error: Python 3 was not configured with SQLite support.  Install 'sqlite-devel' and then re-configure Python 3." >&2
+  echo "  Reference:  http://stackoverflow.com/a/2747227" >&2
+  exit 1
+fi
+
+set -e
+set -x
+
+#Below the 'automake' line are "devel" packages, but we'll break that out after the Hivex revisions are upstream.
+sudo yum install \
+  gcc \
+  gcc-c++ \
+  libxml2-devel \
+  openssl-devel \
+  python-dateutil \
+  python-devel \
+  automake \
+  gettext-devel \
+  libtool \
+  ocaml
---
+++ deps/install_dependent_packages-osx-10.8.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+if [ ! -x `which port` ]; then
+  echo "Error: \`port' not found. Download and install MacPorts." >&2
+  exit 1
+fi
+
+set -e
+set -x
+
+sudo port selfupdate
+sudo port install \
+  autoconf \
+  automake \
+  getopt \
+  libtool \
+  ocaml \
+  pkgconfig \
+  python33
+
+#Thanks to Jim Meyering for the dirlist tip for dealing with PKG_CHECK_MODULES not being found.
+PKGM4=$(find /opt/local /usr -name 'pkg.m4' | head -n1 2>/dev/null)
+if [ ! -r "$PKGM4" ]; then
+  echo "Error: pkg.m4 file not found; hivex will fail to build." >&2
+  exit 1
+fi
+ACLOCALDIRLIST=$(aclocal --print-ac-dir)/dirlist
+if [ ! -r "$ACLOCALDIRLIST" -o ! $(grep "$(dirname "$PKGM4")" "$ACLOCALDIRLIST" | wc -l) -gt 0 ]; then
+  echo "Note: Augmenting aclocal path with m4 directories." >&2
+  sudo bash -c "printf '%s/share/aclocal\n' /opt/local /usr >>$(aclocal --print-ac-dir)/dirlist"
+fi
+test $(cat $(aclocal --print-ac-dir)/dirlist | xargs -I YY find YY -type f 2>/dev/null | grep 'pkg.m4' | wc -l) -gt 0 ;
+echo Done.
