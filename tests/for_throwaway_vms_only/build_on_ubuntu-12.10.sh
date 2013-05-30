97d6e53e936fd5ad4dd63bd890413fcf537c2783
Alex Nelson <a.nelson@prometheuscomputing.com>
2013-05-30 13:19:32 -0400

    Accommodate Ubuntu 12.10's ~/.bashrc like 13.04's

--- tests/for_throwaway_vms_only/build_on_ubuntu-12.10.sh
+++ tests/for_throwaway_vms_only/build_on_ubuntu-12.10.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+echo "Note: Ubuntu 12.10's ~/.bashrc includes an interactive-shell-only directive.  Hence, RegXML Extractor's bashrc modifications are imported into this test script's execution environment directly, and you will need to update your shell's environment (or start a new shell) to inherit the ~/local path updates." >&2
+source ../../deps/bashrc
+
 INSTALL_DEPS=install_dependent_packages-ubuntu-12.10.sh ./_build_regxml_extractor.sh
