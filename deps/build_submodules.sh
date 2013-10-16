2b5df45a4de0a3370a4ecc0f0c7cf5273305aa65
Alex Nelson <a.nelson@prometheuscomputing.com>
2013-10-16 15:25:35 -0400

    OS X, Hivex: Use MacPorts libraries
    This resolves a fresh issue with iconv and OS X's default build.
    
    Thanks to Richard W.M. Jones for the solution.
    
    (cherry picked from commit a5823f310000550ebd182128427d189ce82c208e)
    
    Conflicts:
    	deps/build_submodules.sh
    	tests/for_throwaway_vms_only/build_on_osx-10.7.sh
    	tests/for_throwaway_vms_only/build_on_osx-10.8.sh

--- deps/build_submodules.sh
+++ deps/build_submodules.sh
@@ -4,6 +4,18 @@ get_abspath() {
   python -c 'import os,sys; print(os.path.abspath(os.path.expanduser(sys.argv[1])))' "$1"
 }
 
+#OS X needs a few tweaks to running Hivex's ./configure
+#TODO At least OS X 10.8 needs it.  10.7 and 10.9 might too.  Check.
+hivex_configure_extra_flags=
+if [ ! -z "$RE_HIVEX_CONFIGURE_EXTRA_FLAGS" ]; then
+  hivex_configure_extra_flags="$RE_HIVEX_CONFIGURE_EXTRA_FLAGS"
+else
+  if [ "x$(uname -s)" == "xDarwin" ]; then
+    echo "$0: Error: On OS X 10.8, this script needs an extra environment variable passed in, \$RE_HIVEX_CONFIGURE_EXTRA_FLAGS." >&2
+    exit 1
+  fi
+fi
+
 if [ -z "$PYTHON3" ]; then
   echo "$0: Error: You must specify PYTHON3 as an environment variable in order to build Hivex as RE requires.  For most systems, this can just be 'python3', though on OS X it may need to be 'python3.3' or similar.  Something as simple as this will suffice:" >&2
   echo "" >&2
@@ -43,9 +55,9 @@ pushd $SCRIPTDIR/sleuthkit
 popd
 
 pushd $SCRIPTDIR/hivex
-(./autogen.sh && PYTHON=$PYTHON3 ./configure --disable-ruby --prefix="$INSTALLDIR" --with-python-installdir="$INSTALLDIR/share/hivex/python3" && make -j && $MAKEINSTALL) || exit 1
+(./autogen.sh && PYTHON=$PYTHON3 ./configure --disable-ruby --prefix="$INSTALLDIR" --with-python-installdir="$INSTALLDIR/share/hivex/python3" $hivex_configure_extra_flags && make -j && $MAKEINSTALL) || exit 1
 popd
 
 pushd $SCRIPTDIR/hivex_modified
-(./autogen.sh && PYTHON=$PYTHON3 ./configure --disable-ruby --disable-python --prefix="$INSTALLDIR/share/hivex_modified" && make -j && $MAKEINSTALL) || exit 1
+(./autogen.sh && PYTHON=$PYTHON3 ./configure --disable-ruby --disable-python --prefix="$INSTALLDIR/share/hivex_modified" $hivex_configure_extra_flags && make -j && $MAKEINSTALL) || exit 1
 popd
--- tests/for_throwaway_vms_only/build_on_osx-10.8.sh
+++ tests/for_throwaway_vms_only/build_on_osx-10.8.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-PYTHON3=python3.3 INSTALL_DEPS=install_dependent_packages-osx-10.8.sh ./_build_regxml_extractor.sh
+RE_HIVEX_CONFIGURE_EXTRA_FLAGS="LDFLAGS=-L/opt/local/lib CPPFLAGS=-I/opt/local/include" \
+  PYTHON3=python3.3 \
+  INSTALL_DEPS=install_dependent_packages-osx-10.8.sh \
+  ./_build_regxml_extractor.sh
