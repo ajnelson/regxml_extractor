e58ee796ecb4d700e8d041c2ef0cb17f42f491e5
Alex Nelson <a.nelson@prometheuscomputing.com>
2013-10-17 17:10:11 -0400

    Add scripts for Ubuntu 13.10

---
+++ deps/install_dependent_packages-ubuntu-13.10.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+set -x
+
+#Assume Ubuntu
+#TODO Add a check
+
+sudo apt-get update
+
+#Below the 'automake' line are "devel" packages, but we'll break that out after the Hivex revisions are upstream.
+sudo apt-get install \
+  automake  \
+  g++ \
+  libssl-dev \
+  libtool \
+  libxml2-dev \
+  libxml2-utils \
+  python-dev \
+  sqlite3 \
+  autoconf \
+  autopoint \
+  gettext \
+  libtool \
+  ocaml \
+  python-dateutil
