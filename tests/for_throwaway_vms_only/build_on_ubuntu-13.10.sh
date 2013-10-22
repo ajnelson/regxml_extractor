53c75b84e66fcbf193a9aec7d69ebddb7e3f2a16
Alex Nelson <a.nelson@prometheuscomputing.com>
2013-10-22 15:58:30 -0400

    Import Ubuntu 13.10 scripts from unstable branch

---
+++ tests/for_throwaway_vms_only/build_on_ubuntu-13.10.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Note: Ubuntu 13.10's ~/.bashrc includes an interactive-shell-only directive.  Hence, RegXML Extractor's bashrc modifications are imported into this test script's execution environment directly, and you will need to update your shell's environment (or start a new shell) to inherit the ~/local path updates." >&2
+source ../../deps/bashrc
+cat ../../deps/bashrc >>~/.bashrc
+
+INSTALL_DEPS=install_dependent_packages-ubuntu-13.10.sh ./_build_regxml_extractor.sh
