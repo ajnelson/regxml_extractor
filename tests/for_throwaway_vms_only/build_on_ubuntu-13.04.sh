2d231172cc81ca87a9ed86bd0132e6d2a57abd51
Alex Nelson <a.nelson@prometheuscomputing.com>
2014-06-30 11:21:35 -0400

    Remove EOL'd Ubuntu 13.04

--- tests/for_throwaway_vms_only/build_on_ubuntu-13.04.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Note: Ubuntu 13.04's ~/.bashrc includes an interactive-shell-only directive.  Hence, RegXML Extractor's bashrc modifications are imported into this test script's execution environment directly, and you will need to update your shell's environment (or start a new shell) to inherit the ~/local path updates." >&2
-source ../../deps/bashrc
-cat ../../deps/bashrc >>~/.bashrc
-
-INSTALL_DEPS=install_dependent_packages-ubuntu-13.04.sh ./_build_regxml_extractor.sh
