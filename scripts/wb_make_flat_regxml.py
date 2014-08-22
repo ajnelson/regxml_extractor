e12a24c2ab0c4c27b9933b33aac602f6bb74b506
Alex Nelson <a.nelson@prometheuscomputing.com>
2014-08-22 17:05:35 -0400

    Replace sample rec() with a walk function

--- scripts/wb_make_flat_regxml.py
+++ scripts/wb_make_flat_regxml.py
@@ -3,15 +3,42 @@
 #sudo port install py33-enum34
 
 import sys
+import logging
+import os
+
+_logger = logging.getLogger(os.path.basename(__file__))
+
 from Registry import Registry
 
-reg = Registry.Registry(sys.argv[1])
+def cell_walk(cell_path):
+    """Like os.walk().  However, all lists are not paths, but RegistryKey or RegistryValue objects."""
+    #_logger.debug("cell_walk(%r)" % cell_path)
+    yield (cell_path, cell_path[-1].subkeys(), cell_path[-1].values())
 
-#Print all keys in a Registry
-def rec(key, depth=0):
-    print("\t" * depth + key.path())
+    for key in cell_path[-1].subkeys():
+        yield from cell_walk(cell_path + [key])
 
-    for subkey in key.subkeys():
-        rec(subkey, depth + 1)
+def main():
+    global args
+    reg = Registry.Registry(args.hive_file)
 
-rec(reg.root())
+    for (iter_no, (cell_path, subkeys, values)) in enumerate(cell_walk([reg.root()])):
+        #_logger.debug(cell_path[-1].path())
+
+        #if tally == 1:
+        #    _logger.debug("subkeys = %r." % subkeys)
+        #if tally > 10:
+        #    break
+
+        print(str(cell_path[-1].timestamp()) + "\t" + cell_path[-1].path())
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--debug", action="store_true")
+    parser.add_argument("hive_file")
+    args  = parser.parse_args()
+
+    logging.basicConfig(level=logging.DEBUG if args.debug else logging.INFO)
+
+    main()
